a85e43b7ea3701288f025891c3345cb878ef2085
Min Ho Jang <minho00123@gmail.com>
2024-03-21 00:01:38 +0900

    Refactor: Change store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,22 +3,10 @@ import { create } from "zustand";
 const useStore = create(set => ({
   id: "",
   url: "",
-  urlInfo: {},
-  pingData: {},
-  trafficData: [],
-  bandwidthData: {},
-  tracerouteData: [],
-  isUrlExist: true,
-  showHeaderInput: false,
+  latencies: [],
   setId: id => set({ id }),
   setUrl: url => set({ url }),
-  setUrlInfo: urlInfo => set({ urlInfo }),
-  setPingData: data => set({ pingData: data }),
-  setTrafficData: data => set({ trafficData: data }),
-  setBandwidthData: data => set({ bandwidthData: data }),
-  setTracerouteData: data => set({ tracerouteData: [...data] }),
-  setIsUrlExist: () => set({ isUrlExist: false }),
-  setShowHeaderInput: () => set({ showHeaderInput: true }),
+  setLatencies: data => set({ latencies: [...data] }),
 }));
 
 export default useStore;
