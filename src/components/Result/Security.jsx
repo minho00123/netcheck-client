3f478ace7c8a6ce9bdf03bfd22e814f296ff2243
Min Ho Jang <minho00123@gmail.com>
2024-03-20 23:53:27 +0900

    Refactor: Change Security Component

--- src/components/Result/Security.jsx
+++ src/components/Result/Security.jsx
@@ -1,36 +1,57 @@
+import axios from "axios";
+import useStore from "../../store/store";
+import { useEffect, useState } from "react";
+
 export default function Security() {
+  const { url } = useStore();
+  const [securityData, setSecurityData] = useState({});
+
+  useEffect(() => {
+    async function getData(url) {
+      try {
+        const response = await axios.post(
+          "http://localhost:8000/result/security",
+          { url },
+        );
+
+        setSecurityData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getData(url);
+  }, [url]);
+
   return (
-    <div className="p-4 rounded-xl bg-blue-light shadow-md">
+    <div className="flex flex-col justify-center mr-4 my-5 p-4 rounded-xl bg-blue-light shadow-md">
       <h2 className="text-xl font-bold">Security</h2>
-      <div className="my-5">
-        <div className="rounded-2xl bg-white text-md text-center shadow-md">
-          <h3 className="mx-5 mb-1 pt-2 font-bold text-lg">SSL</h3>
+      <div className="flex mt-2">
+        <div className="mr-4 rounded-2xl bg-white text-md shadow-md">
+          <h3 className="mx-5 mb-1 pt-2 text-lg text-center font-bold">SSL</h3>
           <div className="w-full h-1px bg-gray"></div>
           <p className="mx-5 mt-2 mb-2">
             <span className="text-blue font-bold">Issuer: </span>
-            Google Trust Services LLC
+            {securityData.issuer}
           </p>
           <p className="mx-5 pb-2">
             <span className="text-blue font-bold">Expiry Date: </span>
-            09/14/2028
+            {securityData.expiryDate}
           </p>
         </div>
-        <div className="rounded-2xl bg-white text-md text-center shadow-md">
-          <h3 className="mx-5 mt-3 mb-1 pt-2 font-bold text-lg">
+        <div className="rounded-2xl bg-white text-md shadow-md">
+          <h3 className="mx-5 mt-2 mb-1 text-lg text-center font-bold">
             HTTP Header Analysis
           </h3>
           <div className="w-full h-1px bg-gray"></div>
-          <p className="mx-5 mt-2 mb-2">
-            <span className="text-blue font-bold">HTTPS: </span>
-            True
-          </p>
-          <p className="mx-5 mb-2">
+
+          <p className="mt-2 mx-5 mb-1">
             <span className="text-blue font-bold">CSP: </span>
-            none
+            {securityData.csp ? securityData.csp : "none"}
           </p>
-          <p className="mx-5 pb-2">
+          <p className="mx-5">
             <span className="text-blue font-bold">HSTS: </span>
-            max-age=31536000
+            {securityData.hsts ? securityData.hsts : "none"}
           </p>
         </div>
       </div>
