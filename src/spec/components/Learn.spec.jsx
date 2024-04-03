d8fa265c98c171c873fbdd95d6632085aef16427
Min Ho Jang <minho00123@gmail.com>
2024-04-03 21:55:02 +0900

    Refactor: remove unnecessary test

--- src/spec/components/Learn.spec.jsx
+++ src/spec/components/Learn.spec.jsx
@@ -1,16 +1,7 @@
 import useStore from "../../store/store";
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter, Navigate } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import Learn from "../../components/Info/Learn";
-
-vi.mock("react-router-dom", async importOriginal => {
-  const actual = await importOriginal();
-
-  return {
-    ...actual,
-    Navigate: vi.fn(),
-  };
-});
 
 vi.mock("../../store/store.js", () => ({
   default: vi.fn(),
@@ -25,33 +16,11 @@ describe("Learn component tests", () => {
     }));
 
     render(
-      <BrowserRouter>
+      <MemoryRouter>
         <Learn />
-      </BrowserRouter>,
+      </MemoryRouter>,
     );
 
     expect(screen.getByText("netcheck Features")).toBeInTheDocument();
-  });
-
-  it("Should redirect to /result/:id when the url exists", () => {
-    useStore.mockImplementation(() => ({
-      id: "123",
-      url: "testUrl",
-      setId: vi.fn(),
-    }));
-
-    render(
-      <BrowserRouter>
-        <Learn />
-      </BrowserRouter>,
-    );
-
-    expect(Navigate).toHaveBeenCalledWith(
-      {
-        to: `/result/123`,
-        replace: true,
-      },
-      expect.anything(),
-    );
   });
 });
