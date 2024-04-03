fc458c6b01e86b3461aae980a9c9937597a1a33c
Min Ho Jang <minho00123@gmail.com>
2024-04-03 21:17:15 +0900

    Feat: Add tests

--- src/spec/components/HomeHeader.spec.jsx
+++ src/spec/components/HomeHeader.spec.jsx
@@ -1,16 +1,20 @@
-import { BrowserRouter } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Header from "../../components/Home/Header";
-import { expect } from "vitest";
 
 describe("Home Header component tests", () => {
-  it("Should render properly", () => {
+  beforeEach(() => {
     render(
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>,
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/about" element={<div>About Page</div>} />
+        </Routes>
+      </MemoryRouter>,
     );
+  });
 
+  it("Should render properly", () => {
     const logoImage = screen.getByAltText("logo");
     const aboutLink = screen.getByRole("link", { name: "About" });
 
@@ -21,5 +25,21 @@ describe("Home Header component tests", () => {
     );
     expect(aboutLink).toBeInTheDocument();
     expect(aboutLink.closest("a")).toHaveAttribute("href", "/about");
+  });
+
+  it("Should go to home when clicking the logo", () => {
+    const logo = screen.getByAltText("logo");
+
+    fireEvent.click(logo);
+
+    expect(window.location.pathname).toBe("/");
+  });
+
+  it("Should go to /about when clicking the About button", () => {
+    const aboutButton = screen.getByRole("link", { name: "About" });
+
+    fireEvent.click(aboutButton);
+
+    expect(screen.getByText("About Page")).toBeInTheDocument();
   });
 });
