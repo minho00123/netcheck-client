053a0d2d0b8127361ee37081d917b9e792de3341
Min Ho Jang <minho00123@gmail.com>
2024-04-03 20:36:42 +0900

    Refactor: Use beforeeach

--- src/spec/components/Loading.spec.jsx
+++ src/spec/components/Loading.spec.jsx
@@ -3,15 +3,16 @@ import Loading from "../../components/Common/Loading";
 
 describe("Loading component tests", () => {
   const testText = "Loading...";
-  it("Should render the text correctly", async () => {
-    render(<Loading text={testText} />);
 
+  beforeEach(() => {
+    render(<Loading text={testText} />);
+  });
+
+  it("Should render the text correctly", async () => {
     expect(screen.getByText(testText)).toBeInTheDocument();
   });
 
   it("Should render the loading spinner", () => {
-    render(<Loading text={testText} />);
-
     const spinnerElement = document.querySelector(".animate-spin");
 
     expect(spinnerElement).toBeInTheDocument();
