2eb572d21f64ced4509e3cfd73afcee7bb7895d8
Min Ho Jang <minho00123@gmail.com>
2024-06-14 14:58:54 +0900

    Fix: change websocket address

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -117,7 +117,7 @@ export default function Result() {
 
     getIdData(id);
 
-    const wsUrl = `wss://${seoulServer}`;
+    const wsUrl = `wss://${seoulServer}:443`;
     console.log(`Connecting to WebSocket at ${wsUrl}`);
     ws.current = new WebSocket(wsUrl);
 
