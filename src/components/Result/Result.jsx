dd5fd9743e83344b4d53b7beebf5b0df086fc5e3
Min Ho Jang <minho00123@gmail.com>
2024-06-20 10:26:42 +0900

    Fix: delete unused codes

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -93,7 +93,6 @@ export default function Result() {
           });
         } else {
           getSeoulData(url);
-          getSeoulPingData(url);
           getVirginiaData(url);
           getLondonData(url);
         }
